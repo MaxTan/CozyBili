b4d111729e518a0fe149bd06eb7840921323169a
Max <763305825@qq.com>
2015-06-11 21:06:55 +0800

    添加package

---
+++ CozyBili.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace CozyBili.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
